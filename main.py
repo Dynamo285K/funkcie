751b3744d570fd8b098a7d628823ab8c01e3a909
Dynamo <tomasolbert0412@gmail.com>
2022-10-18 18:16:38 +0200

    pokus o trojuholnik

---
+++ main.py
@@ -0,0 +1,16 @@
+def stars(riadok:int):
+    for i in range(riadok,):
+        for i in range(riadok-i):
+             print(end="")
+        for i in range(5-i,5+i):
+                    print('*')
+
+    print('')
+
+
+
+
+
+
+
+stars(5)
